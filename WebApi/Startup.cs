736eadea2cda60db7354afaed9c8596dfb4bda7d
Boris Arturo González Rivera <borisgr04@gmail.com>
2019-10-31 10:26:11 -0500

    Se confirma OpenApi Swagger

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace WebApi
 {
@@ -34,10 +35,37 @@ namespace WebApi
 
             ///Inyección de dependencia Especifica
             //https://docs.microsoft.com/en-us/aspnet/core/fundamentals/dependency-injection?view=aspnetcore-3.0#register-additional-services-with-extension-methods
-            services.AddScoped<IUnitOfWork, UnitOfWork>(); //Se Instancia un peticion
-            services.AddScoped<IDbContext, BancoContext>(); //Se Instancia un peticion
+            services.AddScoped<IUnitOfWork, UnitOfWork>(); //Crear Instancia por peticion
+            services.AddScoped<IDbContext, BancoContext>(); //Crear Instancia por peticion
 
             services.AddControllers();
+
+
+            #region SwaggerOpen Api
+            //Register the Swagger services
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Task API",
+                    Description = "Task API - ASP.NET Core Web API",
+                    TermsOfService = new Uri("https://cla.dotnetfoundation.org/"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Unicesar",
+                        Email = string.Empty,
+                        Url = new Uri("https://github.com/borisgr04/CrudNgDotNetCore3"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Licencia dotnet foundation",
+                        Url = new Uri("https://www.byasystems.co/license"),
+                    }
+                });
+            });
+
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +81,16 @@ namespace WebApi
             app.UseRouting();
 
             app.UseAuthorization();
+
+            #region 
+            app.UseSwagger();
+            app.UseSwaggerUI(
+                options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Signus Prespuesto v1");
+                }
+            );
+            #endregion
 
             app.UseEndpoints(endpoints =>
             {
